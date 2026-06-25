bec216e1df7c967b36207fec6ee5fe3700107f72
renTHAL-max <athallahsmr1@gmail.com>
2026-06-25 09:02:04 +0700

    Update Juragan Ayam Spicy image to correct URL

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ let products = [
         name: "Juragan Ayam Spicy",
         description: "Ayam goreng pedas dengan sensasi yang menggigit",
         price: 27000,
-        image: "https://i.imgur.com/2mZhAGX.png",
+        image: "https://i.imgur.com/j7lpj9k.png",
         category: "spicy",
         stock: 45
     },
